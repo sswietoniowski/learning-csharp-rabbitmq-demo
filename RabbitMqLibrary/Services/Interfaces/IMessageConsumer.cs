d747f31e92e29c70836150658aa0a4f94d8efb57
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-05-24 08:08:03 +0200

    Code refactored

--- RabbitMqLibrary/Services/Interfaces/IMessageConsumer.cs
+++ RabbitMqLibrary/Services/Interfaces/IMessageConsumer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RabbitMqLibrary.Services.Interfaces;
 
-public interface IMessageConsumer<T> where T : class
+public interface IMessageConsumer<out T> where T : class
 {
     public string ExchangeName { get; set; }
     public string QueueName { get; set; }
